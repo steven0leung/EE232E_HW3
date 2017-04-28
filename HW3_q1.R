b25e83c8bcde168e096bab381c3556f0dffe9acb
steven <steven0leung@ucla.edu>
2017-04-28 12:37:58 -0700

    Q1 to Q3 finished

--- HW3_q1.R
+++ HW3_q1.R
@@ -15,14 +15,48 @@ graph_data <- read.table("sorted_directed_net.txt", sep = "\t", header = FALSE) 
 colnames(graph_data) <- c("Node 1", "Node 2", "weights")
 g1 <- graph.data.frame(graph_data,directed = TRUE) # covert table to directed garph
 
-connectivity <- is.connected(g1) #check if network is connected
+connectivity <- is.connected(g1,mode = "strong") #check if network is connected
 
 if (connectivity == 1) {
   sprintf('The network is connected')
 } else {
   sprintf('The newtork is not connected')
-  cl <- clusters(g1)
+  cl <- clusters(g1, mode = "strong")
   gccIndex <- which.max(cl$csize)
   nonGCCnodes <- (1:vcount(g1))[cl$membership != gccIndex]
   gcc <- delete.vertices(g1,nonGCCnodes)
 }
+
+####################  QUESTION 2 ####################
+
+g1_indeg <- degree(gcc , mode = "in")
+g1_outdeg <- degree(gcc , mode = "out")
+
+hist(g1_indeg, breaks <- seq(from = min(g1_indeg), to = max(g1_indeg), by=1),main = "GCC In-Degree Distribution",xlab = "In-Degree",ylab = "Frequency")
+hist(g1_outdeg, breaks <- seq(from = min(g1_outdeg), to = max(g1_outdeg), by=1),main = "GCC Out-Degree Distribution",xlab = "Out-Degree",ylab = "Frequency")
+
+####################  QUESTION 3 ####################
+
+# option 1 : keep the number of edges unchanged, and just remove the directions
+  # using label.propagation.community
+undirected_1 <- as.undirected(gcc, mode = "each")
+community_1 <- label.propagation.community(undirected_1, weights = E(undirected_1)$weights)
+
+m_1 <- modularity(community_1)
+s_1 <- sizes(community_1)
+
+# option 2 : merge the two directed edges between i and j
+edge_weights <- function(weight) sqrt(prod(weight))
+undirected_2 <- as.undirected(gcc, mode = "collapse", edge.attr.comb = edge_weights)
+
+  # using label.propagation.community
+community_2a <- label.propagation.community(undirected_2, weights = E(undirected_2)$weights)
+m_2a <- modularity(community_2a)
+s_2a <- sizes(community_2a)
+
+  # using fastgreedy.community
+community_2b <- fastgreedy.community(undirected_2, weights = E(undirected_2)$weights)
+m_2b <- modularity(community_2b)
+s_2b <- sizes(community_2b)
+
+
